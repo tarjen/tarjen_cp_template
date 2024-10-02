a633cfb18cd9148687d949981af49532d22c3ee0
tarjen <2679818286@qq.com>
2024-10-02 15:27:06 +0800

    2024.10.2

--- src/string/runs.cpp
+++ src/string/runs.cpp
@@ -1,84 +1,84 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-struct Runs {
-    int l, r, p;
+struct LongestCommonPrefix {
+  int n;
+  vector<int> p, rank;
+  vector<vector<int>> st;
+  LongestCommonPrefix(const string &s) : n(s.size()), p(n), rank(n) {
+    int k = 0;
+    vector<int> q, count;
+    for (int i = 0; i < n; i += 1) p[i] = i;
+    sort(p.begin(), p.end(), [&](int i, int j) { return s[i] < s[j]; });
+    for (int i = 0; i < n; i += 1)
+      rank[p[i]] = i and s[p[i]] == s[p[i - 1]] ? rank[p[i - 1]] : k++;
+    for (int m = 1; m < n; m *= 2) {
+      q.resize(m);
+      for (int i = 0; i < m; i += 1) q[i] = n - m + i;
+      for (int i : p)
+        if (i >= m) q.push_back(i - m);
+      count.assign(k, 0);
+      for (int i : rank) count[i] += 1;
+      for (int i = 1; i < k; i += 1) count[i] += count[i - 1];
+      for (int i = n - 1; i >= 0; i -= 1) p[count[rank[q[i]]] -= 1] = q[i];
+      auto cur = rank;
+      cur.resize(2 * n, -1);
+      k = 0;
+      for (int i = 0; i < n; i += 1)
+        rank[p[i]] = i and cur[p[i]] == cur[p[i - 1]] and
+                             cur[p[i] + m] == cur[p[i - 1] + m]
+                         ? rank[p[i - 1]]
+                         : k++;
+    }
+    st.emplace_back(n);
+    for (int i = 0, k = 0; i < n; i += 1) {
+      if (not rank[i]) continue;
+      k = max(k - 1, 0);
+      int j = p[rank[i] - 1];
+      while (i + k < n and j + k < n and s[i + k] == s[j + k]) k += 1;
+      st[0][rank[i]] = k;
+    }
+    for (int i = 1; (1 << i) < n; i += 1) {
+      st.emplace_back(n - (1 << i) + 1);
+      for (int j = 0; j <= n - (1 << i); j += 1)
+        st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+    }
+  }
+  int get(int i, int j) {
+    if (i == j) return n - i;
+    if (i == n or j == n) return 0;
+    i = rank[i];
+    j = rank[j];
+    if (i > j) swap(i, j);
+    int k = 64 - __builtin_clzll(u64(j - i)) - 1;
+    return min(st[k][i + 1], st[k][j - (1 << k) + 1]);
+  }
 };
-namespace RunsGetter {
-    const int N = 2e5 + 5;
-    using namespace std;
-    struct StrHash{
-        ll p[N],h[N];int mod,buf;
-        StrHash (int _buf,int _mod){buf=_buf;mod=_mod;}
-        void Init(char *s,int n)
-        {
-            p[0]=1;
-            for (int i=1;i<=n;i++){
-                p[i]=p[i-1]*buf%mod;
-                h[i]=(h[i-1]*buf+s[i]-'a'+1)%mod;
-            }
-        }
-        bool tl(int u,int v,int len){
-            return (h[u]-h[v]+(h[v-len]-h[u-len])*p[len])%mod==0;
-        }
-        bool tr(int u,int v,int len){
-            return (h[u+len-1]-h[v+len-1]+(h[v-1]-h[u-1])*p[len])%mod==0;
-        }
-        int get(int l,int r){
-            return (h[r]+(mod-h[l-1])*p[r-l+1])%mod;
-        }
-    }h1(10,1000000007),h2(13,1000000009);
-        int n;char s[N];
-    int extl(int u,int v){
-        if (s[u]!=s[v])return 0;
-        int l=1,r=min(u,v),mid;
-        while(l<r){
-            mid=(l+r+1)>>1;
-            if (h1.tl(u,v,mid)&&h2.tl(u,v,mid))l=mid;
-            else r=mid-1;
-        }return l;
+/*
+    input 0 base
+    output [l,r,p] 1base 
+*/
+vector<tuple<int, int, int>> run(const string &s) {
+  int n = s.size();
+  auto r = s;
+  reverse(r.begin(), r.end());
+  LongestCommonPrefix lcp(s), lcs(r);
+  vector<tuple<int, int, int>> runs;
+  for (bool inv : {false, true}) {
+    vector<int> lyn(n, n), stack;
+    for (int i = 0; i < n; i += 1) {
+      while (not stack.empty()) {
+        int j = stack.back(), k = lcp.get(i, j);
+        if (i + k < n and ((s[i + k] > s[j + k]) ^ inv)) break;
+        lyn[j] = i;
+        stack.pop_back();
+      }
+      stack.push_back(i);
     }
-    int extr(int u,int v)
-    {
-        if (s[u]!=s[v])return 0;
-        int l=1,r=n-max(u,v)+1,mid;
-        while(l<r){
-            mid=(l+r+1)>>1;
-            if (h1.tr(u,v,mid)&&h2.tr(u,v,mid))l=mid;
-            else r=mid-1;
-        }
-        return l;
+    for (int i = 0; i < n; i += 1) {
+      int j = lyn[i], t = j - i, l = i - lcs.get(n - i, n - j),
+          r = j + lcp.get(i, j);
+      if (r - l >= 2 * t) runs.emplace_back(l+1,r,t);
     }
-    Runs b[N*20];
-    bool cmpR(const Runs &A, const Runs &B)
-    {return A.l==B.l ? (A.r==B.r  ? A.p<B.p : A.r<B.r) : A.l<B.l;}
-    int tot;
-    int chk(int u,int v)
-    {
-        int tl=extl(u,v),tr=extr(u,v);
-        if (tl+tr>=v-u+1){
-            b[++tot]=(Runs){u-tl+1,v+tr-1,v-u};
-            return v+tr-1;
-        }
-        return 0;
-    }
-    vector<Runs> getRuns(int n0, char* t0)
-    {
-        n=n0;
-        for(int i=1;i<=n0;i++)s[i]=t0[i];
-        h1.Init(s,n);h2.Init(s,n);
-        for (int p=1;p<=n;p++)
-            for (int k=p,mx=0;k+p<=n;k+=p)
-            if (mx<k)
-                mx=max(mx,chk(k,k+p)-p);
-        sort(b+1,b+tot+1,cmpR);
-        int m=0;
-        for (int i=1;i<=tot;i++)
-            if (b[i].l!=b[i-1].l||b[i].r!=b[i-1].r)
-            b[++m]=b[i];
-        tot=m;
-        vector<Runs> e;
-        for(int i=1;i<=m;i++)e.push_back(b[i]);
-        return e;
-    }
+  }
+  sort(runs.begin(), runs.end());
+  runs.erase(unique(runs.begin(), runs.end()), runs.end());
+  return runs;
 }
